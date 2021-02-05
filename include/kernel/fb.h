b899d5d18408c3316ae85b382674003b9e8c0295
Peter Ho <pho@buhao.cc>
2021-02-05 10:38:18 -0500

    kerror -> kfatal

--- include/kernel/fb.h
+++ include/kernel/fb.h
@@ -8,7 +8,7 @@ extern void kclear_screen(void);
 extern void kputs(char *str);
 extern void kputc(char c);
 extern void kprintf(const char *format, ...);
-extern void kerror(const char *format, ...);
+extern void kfatal(const char *format, ...);
 extern void fb_init();
 
 #ifdef __cplusplus
